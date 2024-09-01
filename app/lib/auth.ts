a54c1c5e732bf30dfeeb95f20e0db34d02de42a7
Andrzej Pietkiewicz <pietan@me.com>
2024-09-01 22:52:35 +0200

    ready app fix

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,15 +1,22 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import { authConfig } from './auth.config';
+import { authConfig } from '../../auth.config';
 import { z } from 'zod';
-import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
+import { prisma } from '@/app/lib/data';
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
+    const user = await prisma.users.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return;
+    }
+    return user;
   } catch (error) {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
